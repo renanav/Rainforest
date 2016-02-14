f53a1d2f0b4cac9fbeedf93023c8927cfd6912e8
marcemc <marcemc@yahoo.com>
2016-02-14 15:09:49 -0500

    Created User model with name, email, password and related to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :users
   get 'images/index'
 
   get 'images/new'
@@ -14,7 +16,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213221728) do
+ActiveRecord::Schema.define(version: 20160214190131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20160213221728) do
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "images", "products"
   add_foreign_key "reviews", "products"
