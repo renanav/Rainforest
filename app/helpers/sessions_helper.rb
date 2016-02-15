5d5d7093c26e91894f6010f7e7ba9b47e67fc285
marcemc <marcemc@yahoo.com>
2016-02-15 17:43:55 -0500

    Finally fixed 'sign out' issues

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,8 +20,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
