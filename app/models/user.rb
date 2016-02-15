719bbe5c41ec2560dbd5c39c13929716e19ba8fc
marcemc <marcemc@yahoo.com>
2016-02-14 19:25:03 -0500

    Added valitadion to User email in 'user.rb'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  validates :name, :email, :password, presence: true,  uniqueness: true
+  validates :name, :email, :password, presence: true, lenght: { maximum: 50 } uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+
   has_many :reviews
 
 end
