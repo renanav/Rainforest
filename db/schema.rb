1a452e070f2cf43139761648d4ae2aef563b159d
renanav <renan.avrahami@gmail.com>
2016-05-21 11:40:00 -0400

    Seed, reposition app/views/model/show.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216015656) do
+ActiveRecord::Schema.define(version: 20160320172608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "category"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "dealership_id"
+    t.integer  "model_id"
+  end
+
+  add_index "cars", ["dealership_id"], name: "index_cars_on_dealership_id", using: :btree
+  add_index "cars", ["model_id"], name: "index_cars_on_model_id", using: :btree
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+  end
+
+  create_table "dealerships", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "email"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "attachment"
@@ -24,6 +51,25 @@ ActiveRecord::Schema.define(version: 20160216015656) do
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
+
+  create_table "makes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "make_id"
+    t.integer  "category_id"
+    t.string   "image"
+    t.integer  "model_id"
+  end
+
+  add_index "models", ["category_id"], name: "index_models_on_category_id", using: :btree
+  add_index "models", ["make_id"], name: "index_models_on_make_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -52,6 +98,10 @@ ActiveRecord::Schema.define(version: 20160216015656) do
     t.string   "remember_digest"
   end
 
+  add_foreign_key "cars", "dealerships"
+  add_foreign_key "cars", "models"
   add_foreign_key "images", "products"
+  add_foreign_key "models", "categories"
+  add_foreign_key "models", "makes"
   add_foreign_key "reviews", "products"
 end
